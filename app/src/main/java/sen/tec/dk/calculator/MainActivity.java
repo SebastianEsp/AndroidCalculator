224d6c3afec5545e42145311e8e4d51f6c479c08
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2015-04-09 10:40:55 +0200

    Updated app to calculate plus, minus, multiplication and division

--- app/src/main/java/sen/tec/dk/calculator/MainActivity.java
+++ app/src/main/java/sen/tec/dk/calculator/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
@@ -13,6 +15,13 @@ public class MainActivity extends ActionBarActivity {
     EditText editText_Num2;
     TextView textView_Result;
     Double result;
+
+    RadioGroup radioGroup_Mode;
+    RadioButton rButton_Plus;
+    RadioButton rButton_Minus;
+    RadioButton rButton_Mult;
+    RadioButton rButton_Divide;
+    String mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +34,34 @@ public class MainActivity extends ActionBarActivity {
         editText_Num1 = (EditText)findViewById(R.id.editTest_Num1);
         editText_Num2 = (EditText)findViewById(R.id.editText_Num2);
         textView_Result = (TextView)findViewById(R.id.textView_Result);
+
+        radioGroup_Mode = (RadioGroup)findViewById(R.id.radioGroup_Mode);
+        radioGroup_Mode.check(R.id.rButton_Plus);
+
+        rButton_Plus = (RadioButton)findViewById(R.id.rButton_Plus);
+        rButton_Minus = (RadioButton)findViewById(R.id.rButton_Minus);
+        rButton_Mult = (RadioButton)findViewById(R.id.rButton_Mult);
+        rButton_Divide = (RadioButton)findViewById(R.id.rButton_Divide);
+    }
+
+    public String getMode()
+    {
+        if (rButton_Plus.isChecked())
+        {
+            return mode = "plus";
+        }
+        else if (rButton_Minus.isChecked())
+        {
+            return mode = "minus";
+        }
+        else if (rButton_Mult.isChecked())
+        {
+            return mode = "mult";
+        }
+        else if (rButton_Divide.isChecked())
+        {
+            return  mode = "divide";
+        }
+        return mode = null;
     }
 }
--- app/src/main/java/sen/tec/dk/calculator/Result_Handler.java
+++ app/src/main/java/sen/tec/dk/calculator/Result_Handler.java
@@ -4,6 +4,7 @@ import android.view.View;
 
 public class Result_Handler implements View.OnClickListener
 {
+    String mode;
     Double num1;
     Double num2;
     MainActivity mainActivity;
@@ -15,12 +16,54 @@ public class Result_Handler implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+
+        mode = mainActivity.getMode();
+
         num1 = Double.valueOf(String.valueOf(mainActivity.editText_Num1.getText()));
         num2 = Double.valueOf(String.valueOf(mainActivity.editText_Num2.getText()));
-        mainActivity.result = num1 + num2;
-        mainActivity.textView_Result.setText(num1 + " + " + num2 + " = " + mainActivity.result);
+
+        if (mode == "plus")
+        {
+            add();
+        }
+        else if (mode == "minus")
+        {
+            sub();
+        }
+        else if (mode == "mult")
+        {
+            mult();
+        }
+        else if (mode == "divide")
+        {
+            div();
+        }
 
         mainActivity.editText_Num1.setText("");
         mainActivity.editText_Num2.setText("");
+    }
+
+    public void add()
+    {
+        mainActivity.result = num1 + num2;
+        mainActivity.textView_Result.setText(num1 + " + " + num2 + " = " + mainActivity.result);
+    }
+
+    public void sub()
+    {
+        mainActivity.result = num1 - num2;
+        mainActivity.textView_Result.setText(num1 + " - " + num2 + " = " + mainActivity.result);
+    }
+
+    public void mult()
+    {
+        mainActivity.result = num1 * num2;
+        mainActivity.textView_Result.setText(num1 + " * " + num2 + " = " + mainActivity.result);
+    }
+
+    public void div()
+    {
+        mainActivity.result = num1 / num2;
+        mainActivity.textView_Result.setText(num1 + " / " + num2 + " = " + mainActivity.result);
     }
 }
